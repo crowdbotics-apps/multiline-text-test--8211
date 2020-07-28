d5daea93eca59be08e9c40e3520ae10c2c5d191f
Crowdbotics <team@crowdbotics.com>
2020-07-28 06:47:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/multiline_text_test__8211/MainActivity.java
@@ -0,0 +1,15 @@
+package com.multiline_text_test__8211;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "multiline_text_test__8211";
+    }
+}
---
+++ backend/multiline_text_test__8211/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for multiline_text_test__8211 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multiline_text_test__8211.settings')
+
+application = get_wsgi_application()
